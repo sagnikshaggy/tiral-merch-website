2f4f5539a6ce387f374947cecce6c109e2aaa483
sagnik-shaggy <sagnik.shaggy@gmail.com>
2022-10-19 18:36:03 -0700

    added sample clothes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">EVE777</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        {data.products.map((product) => (
+          <div>
+            <img src={product.image} alt={product.name} />
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
